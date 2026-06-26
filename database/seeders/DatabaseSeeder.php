5ff7896a8e020bb63202a5562cfb034652764327
Nabilozil7 <nblxdata@gmail.com>
2026-06-26 10:49:48 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\seederProperti;
+use Database\Seeders\seederproperti;
 
 class DatabaseSeeder extends Seeder
 {
